8089e200b8ecacb529ade39527f5c86ebb93ee2f
WIlliam Siqueira Rodriguez <williamsrz@outlook.com>
2019-02-18 08:00:26 -0300

    add new modules

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './features/footer/footer.component';
+import { SidebarComponent } from './features/sidebar/sidebar.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
