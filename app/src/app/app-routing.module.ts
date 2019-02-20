9e336bd24493ef8aeac6589a6806681894105d9b
WIlliam Siqueira Rodriguez <williamsrz@outlook.com>
2019-02-20 09:49:15 -0300

    update partnership and build settings

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'code-of-conduct', component: CodeOfConductComponent },
-  { path: 'speakers', component: SpeakersComponent }
+  { path: 'speakers', component: SpeakersComponent },
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
