7c36c99eb06b2be9d02f79621b6ae512ddebce76
William S. Rodriguez <williamsrz@outlook.com>
2019-04-13 18:32:10 -0300

    added faq

--- app/src/app/features/navbar/navigation.data.ts
+++ app/src/app/features/navbar/navigation.data.ts
@@ -5,7 +5,7 @@ export const NAVPAGES = [
      { visible: false, title: 'PALESTRAS', route: '/talks', description: 'Palestras do evento' },
      { visible: false, title: 'AGENDA', route: '/agenda', description: 'A agenda do evento' },
     // { visible: false, title: 'Patrocínio e Apoio', route: '/partners', description: 'Patrocínio e Apoio' },
-    // { visible: false, title: 'AJUDA', route: '/help', description: 'Precisa de ajuda? Aqui temos algumas respostas' },
+     { visible: false, title: 'AJUDA', route: '/faq', description: 'Precisa de ajuda? Aqui temos algumas respostas' },
     { visible: true, title: 'COC', route: '/code-of-conduct', description: 'Nosso código de conduta' },
     //{ visible: true, title: 'CALL 4 PAPERS', route: 'http://bit.ly/capiconf-2019-c4p', description: '' },
 ];
