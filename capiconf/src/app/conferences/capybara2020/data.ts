11edcd80b8b3e0979f879de108c267ef3990ccc1
William S Rodriguez <williamsrz@outlook.com>
2019-09-24 15:59:38 -0300

    another test version

--- capiconf/src/app/conferences/capybara2020/data.ts
+++ capiconf/src/app/conferences/capybara2020/data.ts
@@ -1,9 +1,9 @@
 export const CONFERENCE = {
 
   name: "#CapiConf 2020",
-  description: "O maior encontro de pessoas desenvolvedoras do Paraná.",
-  highlight: "Dois dias de troca de conhecimento, cultura ediscussões sobre desenvolvimento de software e inovação",
-  date: "14 e 15 de Maio 2020",
+  description: "O maior evento para pessoas desenvolvedoras do Paraná.",
+  highlight: "Dois dias de troca de conhecimento, cultura ediscussões sobre desenvolvimento de software e inovação.",
+  date: "15 e 16 de Maio 2020",
   ticketsUrl: "",
   c4pUrl: "",
   c4SponsorsUrl: ""
