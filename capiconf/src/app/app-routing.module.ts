eff6d830450d0e4b163e64a8fc6757a824c5fba3
William S Rodriguez <williamsrz@outlook.com>
2019-09-26 10:18:59 -0300

    coc and faq

--- capiconf/src/app/app-routing.module.ts
+++ capiconf/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Capybara2020Component } from './conferences/capybara2020/capybara2020.component';
+import { Capybara2020faqComponent } from './conferences/capybara2020faq/capybara2020faq.component';
+import { CocComponent } from './conferences/coc/coc.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/2020/start', pathMatch: 'full' },
+  { path: '', redirectTo: '2020/start', pathMatch: 'full' },
   { path: '2020/start', component: Capybara2020Component },
-  { path: '**', redirectTo: '/2020/start' },
+  { path: '2020/faq', component: Capybara2020faqComponent },
+  { path: 'coc', component: CocComponent },
+  { path: '**', redirectTo: '2020/start' },
 ];
 
 @NgModule({
