5978a0c974396c57e9b8cacc85f2020690418c25
William S Rodriguez <williamsrz@outlook.com>
2020-03-02 13:43:17 -0300

    add partners

---
+++ capiconf/src/app/campaigns/inclua-toda-pessoa/inclua-toda-pessoa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inclua-toda-pessoa',
+  templateUrl: './inclua-toda-pessoa.component.html',
+  styleUrls: ['./inclua-toda-pessoa.component.scss']
+})
+export class IncluaTodaPessoaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
