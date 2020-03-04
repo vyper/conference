a6a6aca35dbfcdecd252ffffcb6b3e3386ac5ad9
William S Rodriguez <williamsrz@outlook.com>
2020-03-04 12:21:46 -0300

    updates

--- capiconf/src/app/campaigns/inclua-toda-pessoa/inclua-toda-pessoa.component.ts
+++ capiconf/src/app/campaigns/inclua-toda-pessoa/inclua-toda-pessoa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta }  from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inclua-toda-pessoa',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncluaTodaPessoaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private metaService: Meta) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Inclua toda pessoa! Não queremos apenas mais uma camiseta de evento! #incluatodapessoa #capiconf');
+    this.metaService.updateTag({ name: 'description', content: 'Queremos uma causa para apoiar, algo maior que uma conferência de pessoas desenvolvedoras, algo maior que o #CapiConf, algo que faça a diferença, que provoque, que faça você pensar, que faça você repensar, que faça você se importar, se posicionar. Toda pessoa deve ser incluída e reconhecida de forma igual.' });
   }
 
 }
